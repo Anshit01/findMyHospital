21c4921808a58b51a6bfcefd44ada199b6858536
Anshit01 <bhardwaj.anshit1379@gmail.com>
2020-12-19 23:32:52 +0530

    changed debug mode

--- findMyHospital/settings.py
+++ findMyHospital/settings.py
@@ -25,7 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'app','templates')
 SECRET_KEY = config.CONFIG_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.DEBUG
 
 ALLOWED_HOSTS = ['*']
 
